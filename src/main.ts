05609196ff0fd187aacc26ca7119e991e316db40
khomanesagar <khomanesagar@gmail.com>
2021-09-22 17:38:54 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import './polyfills';
 // tslint:disable:no-unused-variable
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
@@ -10,17 +9,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-// platformBrowserDynamic().bootstrapModule(AppModule);
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
-
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule);
 
 
 /*
